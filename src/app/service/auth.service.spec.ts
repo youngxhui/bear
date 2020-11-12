28ea2823a462681d5eefe9e766602827d81ccf8d
young <youngxhui@gmail.com>
2020-11-12 16:39:14 +0800

    ✅ 修复 auth.service 中的测试错误

--- src/app/service/auth.service.spec.ts
+++ src/app/service/auth.service.spec.ts
@@ -11,6 +11,9 @@ describe('AuthService', () => {
 
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ5b3VuZ3hodWlAZ21haWwuY29tIiwiaWQiOjEsImV4cCI6MTYwNTIzMjA0MSwiYWNjb3VudCI6InlvdW5ne' +
+    'Gh1aUBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6W3siaWQiOjEsImF1dGhvcml0eSI6IlJPTEVfVVNFUiJ9LHsiaWQiOjIsImF1dGhvcml0eSI6IlJPTEVfQURNSU4ifV0' +
+    'sInVzZXJuYW1lIjoi5byg5LiJIn0.WtEQn9ITzqNTT9ZPT4XhJ5fZnO-vlOmnZBNcwbtwGzU';
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
@@ -18,36 +21,29 @@ describe('AuthService', () => {
     httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(AuthService);
     httpTestingController = TestBed.inject(HttpTestingController);
+    service.setToken(token);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should be set and get Token', () => {
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ5b3VuZ3hodWlAZ21haWwuY29tIiwiaWQiOjEsImV4cCI6MTYwNTIzMjA0MSwiYWNjb3VudCI6InlvdW5ne' +
-      'Gh1aUBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6W3siaWQiOjEsImF1dGhvcml0eSI6IlJPTEVfVVNFUiJ9LHsiaWQiOjIsImF1dGhvcml0eSI6IlJPTEVfQURNSU4ifV0' +
-      'sInVzZXJuYW1lIjoi5byg5LiJIn0.WtEQn9ITzqNTT9ZPT4XhJ5fZnO-vlOmnZBNcwbtwGzU';
-    service.setToken(token);
+  it('should be get Token', () => {
     const t = service.getToken();
     expect(t).toEqual(token);
   });
 
-  it('should get auth', () => {
+  it('should be get auth', () => {
     const user = service.getUser();
     expect(user.id).toEqual(1);
     expect(user.account).toEqual('youngxhui@gmail.com');
   });
 
-  it('should login', () => {
-    const result: Result<string> = new Result<string>();
-    result.data = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ5b3VuZ3hodWlAZ21haWwuY29tIiwiaWQiOjEsImV4cCI6MTYwNTIzMjA0MSwiYWNjb3VudCI6InlvdW5ne' +
-      'Gh1aUBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6W3siaWQiOjEsImF1dGhvcml0eSI6IlJPTEVfVVNFUiJ9LHsiaWQiOjIsImF1dGhvcml0eSI6IlJPTEVfQURNSU4ifV0' +
-      'sInVzZXJuYW1lIjoi5byg5LiJIn0.WtEQn9ITzqNTT9ZPT4XhJ5fZnO-vlOmnZBNcwbtwGzU';
-
+  it('should be login', () => {
+    localStorage.clear();
     const account = 'test@test.com';
     const password = '123456';
     service.login(account, password).subscribe(data =>
-      expect(data).toEqual(result));
+      expect(data.data).toEqual(token));
   });
 });
