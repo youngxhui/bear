ab299819d64ee9b2804a016aaf03cf6e1f2d12df
young <youngxhui@gmail.com>
2020-11-16 09:33:12 +0800

    🎨 格式化代码

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthChildGuard } from './guard/auth-child.guard';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)},
